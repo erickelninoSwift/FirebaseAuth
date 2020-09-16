e947ae24a67f2ea1f02df9f75da2cccb191d788c
Erick Tshimbombo <erick.tshimbombo@yahoo.com>
2020-09-17 00:11:13 +0200

    inital commit

---
+++ Authentification Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Authentification Firebase
+//
+//  Created by El nino Cholo on 2020/09/16.
+//  Copyright © 2020 El nino Cholo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '9.0'
+
+target 'Authentification Firebase' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Authentification Firebase
+  pod 'Firebase'
+  pod 'Firebase/Database'
+  pod 'Firebase/Auth'
+  
+
+end
